fc97f0a8e17bbcea91b8c1627da336fed827c34a
StephKevin <kevinstephano55@gmail.com>
2024-03-09 11:16:10 +0100

    procedure

--- main.c
+++ main.c
@@ -1,26 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-int main()
+void initialiser(int n, int tab[n][n], int j)
 {
-    int n, i, j;
-    printf("Entrez le nombre de lignes : ");
-    scanf("%d", &n);
-    int tab[n][n];
-
     for (j = 0; j <= n; j++)
         {
             tab[j][0]= 1;
             tab[j][j]= 1;
         }
-
-    for (i = 2; i < n; i++)
+}
+void calculer(int n, int tab[n][n],int i, int j)
+{
+   for (i = 2; i < n; i++)
     {
         for (j = 1; j < i; j++)
         {
             tab[i][j] = tab[i - 1][j - 1] + tab[i - 1][j];
         }
     }
+}
+void afficher(int n, int tab[n][n],int i, int j)
+{
     printf("Triangle de Pascal :\n");
     for (i = 0; i < n; i++)
     {
@@ -31,5 +30,15 @@ int main()
         printf("\n");
     }
 
+}
+int main()
+{
+    int n, i, j;
+    printf("Entrez le nombre de lignes : ");
+    scanf("%d", &n);
+    int tab[n][n];
+    initialiser(n, tab, j);
+    calculer(n, tab, i, j);
+    afficher(n, tab, i, j);
     return 0;
 }
